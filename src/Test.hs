5ac2285767f239f12fc76d33b3b8c80a0abc996a
Brent Yorgey <byorgey@cis.upenn.edu>
2010-05-17 19:32:41 +0000

    test bezier curve bounds -- from very limited testing seems to work OK so far.

--- src/Test.hs
+++ src/Test.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies
+           , MultiParamTypeClasses
+           , FlexibleContexts
+           , TypeSynonymInstances
+           , FlexibleInstances #-}
 module Test where
 
 import Diagrams
@@ -7,6 +11,10 @@ import Data.Monoid
 import qualified Data.Map as M
 
 type Point2 = (Double, Double)
+
+instance Transformable Point2 where
+  type TSpace Point2 = Point2
+  transform = aapply
 
 data Printer2D = Printer2D
 
@@ -28,10 +36,10 @@ data Box = Box Point2 Point2 Point2 Point2
 
 instance Transformable Box where
   type TSpace Box = Point2
-  transform a (Box v1 v2 v3 v4) = Box (aapply a v1)
-                                      (aapply a v2)
-                                      (aapply a v3)
-                                      (aapply a v4)
+  transform a (Box v1 v2 v3 v4) = Box (transform a v1)
+                                      (transform a v2)
+                                      (transform a v3)
+                                      (transform a v4)
 
 instance Renderable Box Printer2D where
   render _ (Box v1 v2 v3 v4) = mapM_ print [v1, v2, v3, v4]
@@ -45,3 +53,23 @@ box = Diagram [Prim (Box (0,0) (1,0) (1,1) (0,1))]
                           , (nm "UL", (0,1)) ])
   where boxBounds (x,y) = let d = x*x + y*y
                           in  maximum [x/d, y/d, (x+y)/d, 0]
+
+instance Transformable v => Transformable (Segment v) where
+  type TSpace (Segment v) = TSpace v
+  transform a (Bezier v1 v2 v3) = Bezier (transform a v1)
+                                         (transform a v2)
+                                         (transform a v3)
+
+instance Renderable (Segment Point2) Printer2D where
+  render _ (Bezier c1 c2 x2) = putStrLn "bezier"
+                               >> mapM_ (putStrLn . ("  "++) . show) [c1, c2, x2]
+
+bez2 :: (BSpace b ~ Point2, Renderable (Segment Point2) b)
+     => Point2 -> Point2 -> Point2 -> Diagram b
+bez2 c1 c2 x2 = Diagram [Prim b]
+                        (segmentBounds b)
+                        (M.fromList [ (nm "X1", (0,0))
+                                    , (nm "C1", c1)
+                                    , (nm "C2", c2)
+                                    , (nm "X2", x2) ])
+  where b = Bezier c1 c2 x2
