16751f01beb602852f43582bd8e81ea41e317424
Brent Yorgey <byorgey@cis.upenn.edu>
2010-05-22 01:27:53 +0000

    change from .lhs to .hs, .lhs was silly

---
+++ src/Graphics/Rendering/Diagrams.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, TypeFamilies, MultiParamTypeClasses, UndecidableInstances, GADTs #-}
+
+-- XXX comment me
+
+module Graphics.Rendering.Diagrams where
+
+import Graphics.Rendering.Diagrams.Transform
+import Graphics.Rendering.Diagrams.Expressions
+
+import Data.VectorSpace
+import Data.Basis
+import Data.MemoTrie
+
+import Control.Monad (mapM_)
+
+import qualified Data.Map as M
+
+-- | Abstract diagrams are rendered to particular formats by /backends/.
+--   Each backend must be an instance of the 'Backend' class, and comes
+--   with an associated vector space and rendering environment.
+
+class (HasBasis (BSpace b), HasTrie (Basis (BSpace b))) => Backend b where
+  type BSpace b :: *           -- ^ The vector space associated with this backend
+  type Render b :: *           -- ^ The rendering environment used by this backend
+  type Result b :: *           -- ^ The result of the rendering operation
+  data Option b :: *           -- ^ The rendering options for this backend
+  renderDia     :: b -> [Option b] -> Diagram b -> Result b
+                               -- ^ Render a diagram using this backend
+
+-- | The 'Renderable' type class connects backends to primitives which
+--   they know how to render.
+
+class (Backend b, Transformable t) => Renderable t b where
+  render :: b -> t -> Render b
+  -- ^ Given a token representing the backend and a transformable
+  --   object, render it in the appropriate rendering context.
+
+-- | A value of type @Prim b@ is an opaque (existentially quantified)
+--   primitive which backend @b@ knows how to render.
+
+data Prim b where
+  Prim :: (BSpace b ~ TSpace t, Renderable t b) => t -> Prim b
+
+renderPrim :: b -> Prim b -> Render b
+renderPrim b (Prim t) = render b t
+
+instance Backend b => Transformable (Prim b) where
+  type TSpace (Prim b) = BSpace b
+  transform v (Prim p) = Prim (transform v p)
+
+instance Backend b => Renderable (Prim b) b where
+  render b (Prim p) = render b p
+
+
+type Bounds v = v -> Scalar v
+
+data Diagram b = Diagram { prims  :: [Prim b]
+                         , bounds :: Bounds (BSpace b)
+                         , names  :: NameSet (BSpace b)
+                         }
+
+rebase :: ( Backend b, v ~ BSpace b
+          , InnerSpace v, HasBasis v, HasTrie (Basis v)
+          , AdditiveGroup (Scalar v), Fractional (Scalar v))
+       => LExpr v -> Diagram b -> Diagram b
+rebase e d = Diagram { prims  = map (translate (negateV u))
+                                    (prims d)
+                     , bounds = rebaseBounds u (bounds d)
+                     , names  = M.map (^-^ u) (names d)
+                     }
+  where u = evalLExpr e (names d)
+
+rebaseBounds :: (InnerSpace v, AdditiveGroup (Scalar v), Fractional (Scalar v))
+             => v -> Bounds v -> Bounds v
+rebaseBounds u f v = f v ^-^ ((u ^/ (v <.> v)) <.> v)
+
+instance ( Backend b
+         , HasTrie (Basis (BSpace b))
+         , HasBasis (BSpace b)) =>
+         Transformable (Diagram b) where
+  type TSpace (Diagram b) = BSpace b
+  transform t (Diagram ps bs ns) = Diagram (map (transform t) ps)
+                                           (\v -> undefined)    -- XXX
+                                           (M.map (aapply t) ns)
+
+atop :: Ord (Scalar (BSpace b)) => Diagram b -> Diagram b -> Diagram b
+atop (Diagram ps1 bs1 ns1) (Diagram ps2 bs2 ns2) =
+  Diagram (ps1 ++ ps2)
+          (\v -> max (bs1 v) (bs2 v))  -- XXX make this nicer?
+          (M.union ns1 ns2)
+
+beside :: ( Backend b
+          , v ~ BSpace b
+          , HasBasis v
+          , HasTrie (Basis v)
+          , InnerSpace v
+          , AdditiveGroup (Scalar v)
+          , Fractional (Scalar v)
+          , Ord (Scalar v))
+       => v -> Diagram b -> Diagram b -> Diagram b
+beside v d1 d2 = rebase (Const (bounds d1 v *^ v)) d1
+          `atop` rebase (Const (bounds d2 (negateV v) *^ negateV v)) d2
---
+++ src/Graphics/Rendering/Diagrams/Transform.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE TypeOperators, FlexibleContexts, UndecidableInstances, TypeFamilies #-}
+
+-- XXX comment me!
+
+module Graphics.Rendering.Diagrams.Transform where
+
+import Data.AdditiveGroup
+import Data.VectorSpace
+import Data.LinearMap
+import Data.Basis
+import Data.MemoTrie
+
+import Data.Monoid
+
+-- | An affine transformation consists of a linear transformation
+--   followed by a translation.
+
+data Affine v = Affine { getLinear    :: v :-* v
+                       , getTranslate :: v
+                       }
+
+-- | Affine transformations are closed under composition.
+
+instance (HasBasis v, HasTrie (Basis v), VectorSpace v)
+         => Monoid (Affine v) where
+  mempty  = Affine idL zeroV
+  mappend (Affine a2 b2) (Affine a1 b1) =
+    Affine (a2 *.* a1) (lapply a2 b1 ^+^ b2)
+
+-- | Apply an affine transformation.
+
+aapply :: (HasBasis v, HasTrie (Basis v)) => Affine v -> v -> v
+aapply (Affine a b) v = lapply a v ^+^ b
+
+-- | Treat a linear transformation as an affine transformation.
+
+fromLinear :: AdditiveGroup v => (v :-* v) -> Affine v
+fromLinear t = Affine t zeroV
+
+-- | Treat a translation as an affine transformation.
+
+fromTranslate :: (HasBasis v, HasTrie (Basis v)) => v -> Affine v
+fromTranslate = Affine idL
+
+-- | Type class for things which can be transformed by affine
+--   transformations.
+
+class (HasBasis (TSpace t), HasTrie (Basis (TSpace t))) => Transformable t where
+  type TSpace t :: *         -- ^ Vector space of transformations
+  transform :: Affine (TSpace t) -> t -> t
+
+translate :: Transformable t => TSpace t -> t -> t
+translate = transform . fromTranslate
+
+scale :: Transformable t => Scalar (TSpace t) -> t -> t
+scale = transform . fromLinear . linear . (*^)
