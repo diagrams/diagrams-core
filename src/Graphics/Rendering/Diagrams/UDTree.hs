e278dc5765b100645d88fb7cab3662e30451114f
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-03 20:23:27 +0000

    more work on UDTree

--- src/Graphics/Rendering/Diagrams/UDTree.hs
+++ src/Graphics/Rendering/Diagrams/UDTree.hs
@@ -5,56 +5,85 @@ import Control.Arrow
 
 import qualified Data.Set as S
 
-data UDTree u d a = Leaf u a
-                  | Branch u [UDTree u d a]
-                  | UNode (u -> u) (UDTree u d a)
-                  | DNode d u (UDTree u d a)
+-- Abstractly, a UDTree is a rose tree with data at the leaves and two
+-- monoidal annotations at every internal node, one of type u and one
+-- of type d, such that
+--
+--   * every u annotation is equal to the mconcat of all u annotations
+--     _below_ it in the tree (in order from left to right), and
+--
+--   * every d annotation is equal to the mconcat of all d annotations
+--     _above_ it in the tree (in order from top to bottom).
+--
+-- When placing a new parent node over a list of subtrees, you may
+-- provide a d annotation for it (otherwise it will be taken to be
+-- mempty); all the d annotations on the rest of the tree will be
+-- updated appropriately.
+--
+-- You may also map a function f over all the u or d annotations, as
+-- long as f is a monoid homomorphism, that is, f (u1 <> u2) = f u1 <>
+-- f u2.
 
-getU :: UDTree u d a -> u
-getU (Leaf    u _) = u
+
+-- For efficiency, we do not actually store d annotations at every
+-- node, and we delay applying homomorphisms until actually descending
+-- into the tree.
+
+data UDTree u d a = Leaf a                         -- ^ A piece of data.
+                  | Branch u [UDTree u d a]        -- ^ A branch point, with a cached
+                                                   --   @u@ value.
+                  | UMap (u -> u) (UDTree u d a)   -- ^ A delayed @u@ homomorphism.
+                  | DMap (d -> d) u (UDTree u d a) -- ^ A delayed @d@ homomorphism,
+                                                   --   with cached @u@ value copied
+                                                   --   from below.
+                  | DNode d u (UDTree u d a)       -- ^ A specified @d@ value,
+                                                   --   with cached @u@ value.
+
+
+-- | Get the @u@ annotation at the root of a tree.
+getU :: Monoid u => UDTree u d a -> u
+getU (Leaf    _)   = mempty
 getU (Branch  u _) = u
-getU (UNode   f t) = f (getU t)
+getU (UMap  f t)   = f (getU t)
+getU (DMap _ u _)  = u
 getU (DNode _ u _) = u
 
+-- | Create a branch node with the given children, whose @d@
+--   annotation is @mempty@ and @u@ annotation is computed
+--   automatically from the annotations on the children.
 branch :: Monoid u => [UDTree u d a] -> UDTree u d a
 branch ts = Branch (mconcat . map getU $ ts) ts
 
-setD :: d -> UDTree u d a -> UDTree u d a
-setD d t = DNode d (getU t) t
+-- | Apply a new @d@ annotation at the root, combining it (on the
+--   left) with the existing @d@ annotation.
+applyD :: d -> UDTree u d a -> UDTree u d a
+applyD d t = DNode d (getU t) t
 
-modifyU :: (u -> u) -> UDTree u d a -> UDTree u d a
-modifyU f t = UNode f t
-
+-- | Map a function over all the @u@ annotations.  The function must
+--   be a monoid homomorphism.
 mapU :: (u -> u) -> UDTree u d a -> UDTree u d a
-mapU f (Leaf u a) = Leaf (f u) a
-mapU f (Branch u ts) = Branch (f u) (map (mapU f) ts)
-mapU f (UNode g t) = UNode (f . g) t
-mapU f (DNode d u t) = DNode d (f u) (mapU f t)
+mapU f t = UMap f t
 
-flatten :: Monoid d => UDTree u d a -> [(a,d)]
-flatten (Leaf _ a) = [(a, mempty)]
-flatten (Branch _ ts) = concatMap flatten ts
-flatten (UNode _ t) = flatten t
-flatten (DNode d _ t) = (map . second) (d `mappend`) (flatten t)
+-- | Map a function over all the @d@ annotations.  The function must
+--   be a monoid homomorphism.
+mapD :: (d -> d) -> UDTree u d a -> UDTree u d a
+mapD f t = DMap f t
+
+-- | Flatten a tree into a list of leaves along with their @d@ annotations.
+-- flatten :: Monoid d => UDTree u d a -> [(a,d)]
+-- flatten (Leaf a) = [(a, mempty)]
+-- flatten (Branch _ ts) = concatMap flatten ts
+-- flatten (UMap _ t) = flatten t
+-- flatten (DMap f _ t) = (map . second)
+--   (map . second) (d `mappend`) (flatten t)
 
 t = setD (Product 5) $ branch [Leaf (Sum 2) 'x', Leaf (Sum 4) 'y', setD (Product 2) $ Leaf (Sum 5) 'z']
 
 t2 = branch [modifyU (const $ Sum 6) t, Leaf (Sum 1) 'q']
 
--- Some sort of fold for processing this?  Essentially we want to be
--- able to do something at each DNode and at each Leaf.  But we may
--- want to do something on the way through the DNode AND on the way
--- "back out", to restore something etc.  Ah, but that's OK, because
--- with the fold your function is passed the result from folding the
--- subtree, so you can append stuff both before and after it.
 
--- Maybe at each DNode you want
--- to be given the current accumulated d value as well as the d value
--- at the node itself, and for leaves you are given the accumulated d
--- value.
-
-foldD :: (Monoid r, Monoid d)
-      => (d -> d -> r -> r) -> (d -> a -> r) -> UDTree u d a -> r
+foldUD :: (Monoid r, Monoid d)
+      => (u -> d -> d -> r -> r) -> UDTree u d a -> r
 foldD = foldD' mempty
 
 foldD' :: (Monoid r, Monoid d)
