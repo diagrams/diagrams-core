aa3660b88f13de42882eb67bfc50465de14d293b
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-18 19:44:45 +0000

    documentation for HasOrigin

--- src/Graphics/Rendering/Diagrams/HasOrigin.hs
+++ src/Graphics/Rendering/Diagrams/HasOrigin.hs
@@ -10,7 +10,7 @@
 -- Maintainer  :  diagrams-discuss@googlegroups.com
 --
 -- Types which have an intrinsic notion of a \"local origin\",
--- i.e. things which are NOT invariant under translation.
+-- /i.e./ things which are /not/ invariant under translation.
 --
 -----------------------------------------------------------------------------
 
@@ -27,22 +27,42 @@ import Data.VectorSpace
 -- | Class of types which have an intrinsic notion of a \"local
 --   origin\", i.e. things which are not invariant under translation,
 --   and which allow the origin to be moved.
+--
+--   One might wonder why not just use 'Transformable' instead of
+--   having a separate class for 'HasOrigin'; indeed, for types which
+--   are instances of both we should have the identity
+--
+--   > moveOriginTo (origin .^+ v) === translate (negateV v)
+--
+--   The reason is that some things (e.g. vectors, 'Trail's) are
+--   transformable but are translationally invariant, i.e. have no
+--   origin.
 class VectorSpace (V t) => HasOrigin t where
 
   -- | Move the local origin to another point.
+  --
+  --   Note that this function is in some sense dual to 'translate'
+  --   (for types which are also 'Transformable'); moving the origin
+  --   itself while leaving the object \"fixed\" is dual to fixing the
+  --   origin and translating the diagram.
   moveOriginTo :: Point (V t) -> t -> t
 
 -- | Move the local origin by a relative vector.
 moveOriginBy :: HasOrigin t => (V t) -> t -> t
 moveOriginBy = moveOriginTo . P
 
--- | Apply the translation that sends the origin to the given point.
---   Note that this is dual to 'moveOriginTo'.  In 'moveOriginTo' we
---   think of fixing the diagram and moving the origin.  In 'moveTo'
---   we think of fixing the origin and moving the diagram.
+-- | Translate the object by the translation that sends the origin to
+--   the given point. Note that this is dual to 'moveOriginTo', i.e. we
+--   should have
+--
+--   > moveTo (origin .^+ v) === moveOriginTo (origin .^- v)
+--
+--   For types which are also 'Transformable', this is essentially the
+--   same as 'translate', i.e.
+--
+--   > moveTo (origin .^+ v) === translate v
 moveTo :: HasOrigin t => Point (V t) -> t -> t
 moveTo = moveOriginBy . (origin .-.)
-
 
 instance VectorSpace v => HasOrigin (Point v) where
   moveOriginTo (P u) p = p .-^ u
