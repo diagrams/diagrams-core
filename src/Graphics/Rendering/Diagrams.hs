5ff4fa3937967d17cfff5c9b664698f8947be56b
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-25 19:29:38 +0000

    update documentation for G.R.Diagrams

--- src/Graphics/Rendering/Diagrams.hs
+++ src/Graphics/Rendering/Diagrams.hs
@@ -7,27 +7,17 @@
 --
 -- The core library of primitives forming the basis of an embedded
 -- domain-specific language for describing and rendering diagrams.
--- For most library-level and user-level code, it should be sufficient
--- to import this module, which simply re-exports useful functionality
--- from other modules in the core library.  Library writers needing
--- finer-grained access or functionality may occasionally find it
--- useful to directly import one of the constituent core libraries:
+-- Normal users of the diagrams library should almost never need to
+-- import anything from this package directly; instead, import modules
+-- (especially "Diagrams.Prelude") from the diagrams-lib package,
+-- which re-exports most things of value to users.
 --
---   * "Graphics.Rendering.Diagrams.Transform" for invertible linear
---     and projective transformations
---
---   * "Graphics.Rendering.Diagrams.Points" for points (as distinct from vectors)
---
---   * "Graphics.Rendering.Diagrams.Names" for names used to identify locations
---     within diagrams
---
---   * "Graphics.Rendering.Diagrams.Bounds" for functional bounding regions.
---
---   * "Graphics.Rendering.Diagrams.HasOrigin" for a type class governing things
---     with an intrinsic notion of a \"local origin\".
---
---   * "Graphics.Rendering.Diagrams.Basics" for backends and the
---     'Diagram' type itself.
+-- For most library code needing access to core internals, it should
+-- be sufficient to import this module, which simply re-exports useful
+-- functionality from other modules in the core library.  Library
+-- writers needing finer-grained access or functionality may
+-- occasionally find it useful to directly import one of the
+-- constituent core modules.
 --
 -----------------------------------------------------------------------------
 
@@ -64,12 +54,6 @@ module Graphics.Rendering.Diagrams
 
        , lookupN
 
-         -- * Backends
-
-       , Backend(..)
-       , MultiBackend(..)
-       , Renderable(..)
-
          -- * Attributes and styles
 
        , AttributeClass
@@ -79,11 +63,7 @@ module Graphics.Rendering.Diagrams
        , getAttr, setAttr, addAttr
        , applyAttr
 
-         -- * Primtives
-
-       , Prim(..), nullPrim
-
-         -- * Bounds
+         -- * Bounding regions
 
        , Bounds(..)
        , Boundable(..)
@@ -94,6 +74,14 @@ module Graphics.Rendering.Diagrams
 
        , HasOrigin(..), moveOriginBy
 
+         -- * Queries
+
+       , Query(..)
+
+         -- * Primtives
+
+       , Prim(..), nullPrim
+
          -- * Diagrams
 
        , AnnDiagram, mkAD, Diagram
@@ -102,15 +90,15 @@ module Graphics.Rendering.Diagrams
 
        , named
 
+       , freeze
+
        , atop
 
-         -- * Queries
+         -- * Backends
 
-       , Query(..)
-
-         -- ** Freezing and thawing
-
-       , freeze
+       , Backend(..)
+       , MultiBackend(..)
+       , Renderable(..)
 
        ) where
 
