diagrams/diagrams-core

commit 0681b24b3d5c7294f2a960fb5d76eee5e6f378ec

Better definition for diameter

src/Diagrams/Core/Envelope.hs
@@ -227,7 +227,9 @@ envelopeP v = P . envelopeV v
 -- | Compute the diameter of a enveloped object along a particular
 --   vector.  Returns zero for the empty envelope.
 diameter :: Enveloped a => V a -> a -> Scalar (V a)
-diameter v a = magnitude (envelopeV v a ^-^ envelopeV (negateV v) a)
+diameter v a = case appEnvelope $ getEnvelope a of
+  (Just env) -> (env v - env (negateV v)) * magnitude v
+  Nothing -> 0
 
 -- | Compute the \"radius\" (1\/2 the diameter) of an enveloped object
 --   along a particular vector.

commit f3a216e972e25055bf8bb324c9d40ad418f58c7e

bug fix: compute diameter correctly: add scalars instead of subtracting
Fixes #35. This one was sneaky.  The bug was introduced in
https://github.com/diagrams/diagrams-core/commit/0681b24b3d5c7294f2a960fb5d76eee5e6f378ec,
which looks innocuous on the surface -- but there's a big difference
between subtracting two *vectors* (what the original code did) and
subtracting two *scalars* (which is what the code does now).  What we
actually want to do is *add* the two scalars.

src/Diagrams/Core/Envelope.hs
@@ -251,7 +251,7 @@ envelopeS v = fromMaybe 0 . envelopeSMay v
 --   vector.  Returns zero for the empty envelope.
 diameter :: Enveloped a => V a -> a -> Scalar (V a)
 diameter v a = case appEnvelope $ getEnvelope a of
-  (Just env) -> (env v - env (negateV v)) * magnitude v
+  (Just env) -> (env v + env (negateV v)) * magnitude v
   Nothing -> 0
 
 -- | Compute the \"radius\" (1\/2 the diameter) of an enveloped object