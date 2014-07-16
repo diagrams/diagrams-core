9c9972a510ddba244175712ad45cbcabc19871f2
Brent Yorgey <byorgey@gmail.com>
2014-07-16 10:52:26 -0400

    work on Contextual-izing Juxtapose (one missing method)

--- src/Diagrams/Core/Juxtapose.hs
+++ src/Diagrams/Core/Juxtapose.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE FlexibleContexts
-           , UndecidableInstances
-           , TypeFamilies
-  #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE UndecidableInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Diagrams.Core.Juxtapose
@@ -18,12 +17,15 @@ module Diagrams.Core.Juxtapose
        ( Juxtaposable(..), juxtaposeDefault
        ) where
 
-import           Data.Functor ((<$>))
-import qualified Data.Map as M
-import qualified Data.Set as S
+import           Control.Applicative
+import           Control.Lens            (review)
+import           Data.Functor            ((<$>))
+import qualified Data.Map                as M
+import qualified Data.Set                as S
 
 import           Data.VectorSpace
 
+import           Diagrams.Core.Context
 import           Diagrams.Core.Envelope
 import           Diagrams.Core.HasOrigin
 import           Diagrams.Core.V
@@ -38,18 +40,18 @@ class Juxtaposable a where
   --   @a2@; @a1@'s local origin becomes @a2@'s new local origin.  The
   --   result is just a translated version of @a2@.  (In particular,
   --   this operation does not /combine/ @a1@ and @a2@ in any way.)
-  juxtapose :: V a -> a -> a -> a
+  juxtapose :: V a -> a -> a -> Contextual (V a) a
 
 -- | Default implementation of 'juxtapose' for things which are
 --   instances of 'Enveloped' and 'HasOrigin'.  If either envelope is
 --   empty, the second object is returned unchanged.
-juxtaposeDefault :: (Enveloped a, HasOrigin a) => V a -> a -> a -> a
-juxtaposeDefault v a1 a2 =
+juxtaposeDefault :: (Enveloped a, HasOrigin a) => V a -> a -> a -> Contextual (V a) a
+juxtaposeDefault v a1 a2 = do
+  mv1 <- (fmap . fmap) negateV (envelopeVMay v a1)
+  mv2 <- envelopeVMay (negateV v) a2
   case (mv1, mv2) of
-    (Just v1, Just v2) -> moveOriginBy (v1 ^+^ v2) a2
-    _                  -> a2
-  where mv1 = negateV <$> envelopeVMay v a1
-        mv2 = envelopeVMay (negateV v) a2
+    (Just v1, Just v2) -> return $ moveOriginBy (v1 ^+^ v2) a2
+    _                  -> return a2
 
 instance (InnerSpace v, OrderedField (Scalar v)) => Juxtaposable (Envelope v) where
   juxtapose = juxtaposeDefault
@@ -68,4 +70,7 @@ instance (Enveloped b, HasOrigin b, Ord b) => Juxtaposable (S.Set b) where
   juxtapose = juxtaposeDefault
 
 instance Juxtaposable a => Juxtaposable (b -> a) where
-  juxtapose v f1 f2 b = juxtapose v (f1 b) (f2 b)
+  juxtapose v f1 f2 = undefined
+  -- XXX how to implement this?
+
+  -- old code:  juxtapose v (f1 b) (f2 b)
